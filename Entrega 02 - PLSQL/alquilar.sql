77031aa1414d87168db120314bf9d57a71d3ec87
Jorge <jrg1011@alu.ubu.es>
2021-03-31 23:34:20 +0200

    nose xd

--- Entrega 02 - PLSQL/alquilar.sql
+++ Entrega 02 - PLSQL/alquilar.sql
@@ -31,11 +31,15 @@ create or replace procedure alquilar(arg_NIF_cliente varchar,
 
 
     -- Declaración de Variables:
-    fecha_inicial   date;
-    fecha_final     date;
-    vehiculo_a_reservar type_modelo_reserva;
-    num_dias integer;
-    reserva_actual reservas.idreserva%type;
+    fecha_inicial            date;
+    fecha_final              date;
+    vehiculo_a_reservar      type_modelo_reserva;
+    num_dias                 integer;
+    reserva_actual           reservas.idreserva%type;
+
+    -- Facturas:
+    precio_total_combustible NUMBER;
+    precio_total_alquiler    NUMBER;
 
     -- Declaración de Cursores
     -- Req. 2
@@ -66,7 +70,9 @@ begin
     fecha_inicial := trunc(arg_fecha_ini, 'DDD');
     fecha_final := trunc(arg_fecha_fin, 'DDD');
 
-    if fecha_inicial > fecha_final then
+    num_dias := (fecha_final - fecha_inicial);
+
+    if num_dias < 0 then
         raise NEGATIVE_DATES_DIFF;
     end if;
 
@@ -98,7 +104,32 @@ begin
     -- Requisito 5: Facturas
     --insert into facturas values (SEQ_NUM_FACT.nextval,);
 
+    -- OJO!: No volver a hacer fetch de los valores del vehículo porque ya se encuentran en la variable "vehículo_a_reservar"
+
+
+    -- Requisito 5.1 Primera línea de factura
+    precio_total_alquiler := num_dias * vehiculo_a_reservar.precio_cada_dia;
+
+    insert into LINEAS_FACTURA
+    values (SEQ_NUM_FACT.nextval,
+            num_dias || ' días de alquiler vehiculo modelo ' || vehiculo_a_reservar.modelo,
+            precio_total_alquiler);
+
+
+    -- Requisito 5.2 Segunda línea de factura
+    precio_total_combustible := vehiculo_a_reservar.capacidad_deposito * vehiculo_a_reservar.precio_por_litro;
+
+    insert into LINEAS_FACTURA
+    values (SEQ_NUM_FACT.currval,
+            'Deposito lleno (' || vehiculo_a_reservar.capacidad_deposito || ' litros de ' ||
+            vehiculo_a_reservar.tipo_combustible,
+            precio_total_combustible);
     commit;
+
+    -- Requisito 5: Factura y Total
+
+    insert into FACTURAS values (SEQ_NUM_FACT.currval, precio_total_combustible + precio_total_alquiler, arg_NIF_cliente);
+
 
 exception
     when NEGATIVE_DATES_DIFF then
