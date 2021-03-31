28369bde0fb417fc17c2bd51c4c3fea20c1885c0
Jorge <jrg1011@alu.ubu.es>
2021-03-31 14:50:51 +0200

    Caso 1 completado; Caso dos en proceso

--- Entrega 02 - PLSQL/alquilar.sql
+++ Entrega 02 - PLSQL/alquilar.sql
@@ -1,20 +1,63 @@
-/*
-    El objetivo de esta función es el de separar los procedimientos de los tests
-    y tablas.
-
-    Cuando lo tengamos hecho lo copiamos y pegamos directamente sobre el script principal,
-    pero así es más fácil navegar por el código.
-*/
 
 
 create or replace procedure alquilar(arg_NIF_cliente varchar,
-  arg_matricula varchar, arg_fecha_ini date, arg_fecha_fin date) is
+                                     arg_matricula varchar,
+                                     arg_fecha_ini date,
+                                     arg_fecha_fin date) is
+    -- Declaración de Excepciones
+    NEGATIVE_DATES_DIFF exception; -- Req. 1
+    pragma exception_init ( NEGATIVE_DATES_DIFF, -20003 );
+
+    -- Declaración de Estructuras:
+    modelo_fila MODELOS%rowtype;
+    -- Declaración de Variables:
+    fecha_ini date;
+    fecha_fin date;
+
+    modelo_vehiculo number;
+
+
+    -- Declaración de Cursores
+    cursor obtener_modelo is
+        select NOMBRE, PRECIO_CADA_DIA, CAPACIDAD_DEPOSITO, PC.TIPO_COMBUSTIBLE, PRECIO_POR_LITRO
+        from MODELOS MOD join PRECIO_COMBUSTIBLE PC on MOD.TIPO_COMBUSTIBLE = PC.TIPO_COMBUSTIBLE
+                         join VEHICULOS on VEHICULOS.ID_MODELO= MOD.ID_MODELO
+        where MATRICULA = arg_matricula;
+
+
+
 begin
-null;
+
+
+
+    -- Requisito 1: Comprobar Fechas:
+    -- Limpiamos las fechas con TRUNC
+    fecha_ini := trunc(arg_fecha_ini, 'DDD');
+    fecha_fin := trunc(arg_fecha_fin, 'DDD');
+
+    if fecha_ini > fecha_fin then
+        raise NEGATIVE_DATES_DIFF;
+    end if;
+
+
+    -- Requisito 2: Modelo Vehículo:
+    open obtener_modelo;
+    fetch obtener_modelo into modelo_fila;
+    close obtener_modelo;
+
+exception
+    when NEGATIVE_DATES_DIFF then
+        rollback;
+        raise_application_error(-20003, 'El numero de dias sera mayor que cero.');
+
+    when OTHERS then
+        -- TODO: Tratamiento genérico:
+        rollback;
+        raise;
 end;
 /
 
-
 -- Ejecución de los tests
-set serveroutput on
-exec test_alquila_coches;
+begin
+    TEST_ALQUILA_COCHES();
+end;
