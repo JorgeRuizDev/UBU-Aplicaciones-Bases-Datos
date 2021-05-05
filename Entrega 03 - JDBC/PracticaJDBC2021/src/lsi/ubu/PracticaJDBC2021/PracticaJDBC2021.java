9ab37ff9168fb0fa6bf08069316c72db854270c5
Jorge <jrg1011@alu.ubu.es>
2021-05-05 23:36:34 +0200

    Más o menos en progreso, todos los test OK pero con dudas

--- Entrega 03 - JDBC/PracticaJDBC2021/src/lsi/ubu/PracticaJDBC2021/PracticaJDBC2021.java
+++ Entrega 03 - JDBC/PracticaJDBC2021/src/lsi/ubu/PracticaJDBC2021/PracticaJDBC2021.java
@@ -16,120 +16,188 @@ import org.slf4j.LoggerFactory;
 
 public class PracticaJDBC2021 {
 
+	private static final String script_path = "sql/";
 	private static PoolDeConexiones p = null;
 	private static Logger l = null;
 
-	private static final String script_path = "sql/";
-    
-    public static void main(String [] argv) {
-    	inicializaciones();
+	public static void main(String[] argv) {
+		inicializaciones();
 		pruebaAltaAutorizado();
 
 		System.out.println("FIN.............");
 	}
-    
-    static void pruebaAltaAutorizado() {
-		
+
+	static void pruebaAltaAutorizado() {
+
 		creaTablas();
-		
+
 		//Pruebo a dar de alta un autorizado que no existe
 		//Doy de alta en la cuenta 1 al cliente con DNI 44444 que no existe
-	    try{        	
-	    	altaAutorizado(1, 44444);
-	    	System.out.println("Cliente inexistente MAL");
-	     } catch (SQLException_PracticaJDBC2021 e) {
-			if (e.getErrorCode() == SQLException_PracticaJDBC2021.NO_EXISTE_CLIENTE)
+		try {
+			altaAutorizado(1, 44444);
+			System.out.println("Cliente inexistente MAL");
+		} catch (SQLException_banco e) {
+			if (e.getErrorCode() == SQLException_banco.NO_EXISTE_CLIENTE)
 				System.out.println("Cliente inexistente OK");
 			else
 				System.out.println("Cliente inexistente MAL");
 		}
-	    System.out.println("--------------------------------");  
-	    
-	  //Pruebo a dar de alta un autorizado en una cuenta que no existe
-	  //cuenta=2 y el dni=12345678  
-	    try{        	
-	    	altaAutorizado(2, 12345678);
-	    	System.out.println("Cuenta inexistente MAL");
-	     } catch (SQLException_PracticaJDBC2021 e) {
-			if (e.getErrorCode() == SQLException_PracticaJDBC2021.NO_EXISTE_CTA)
-				System.out.println("Cuenta inexistente OK");			
+		System.out.println("--------------------------------");
+
+		//Pruebo a dar de alta un autorizado en una cuenta que no existe
+		//cuenta=2 y el dni=12345678
+		try {
+			altaAutorizado(2, 12345678);
+			System.out.println("Cuenta inexistente MAL");
+		} catch (SQLException_banco e) {
+			if (e.getErrorCode() == SQLException_banco.NO_EXISTE_CTA)
+				System.out.println("Cuenta inexistente OK");
 			else
 				System.out.println("Cuenta inexistente MAL");
 		}
-	    System.out.println("--------------------------------");
-	    //Caso correcto
-	    //cuenta=1 y el dni=12345678
-	    
-	    Connection conn = null;
+		System.out.println("--------------------------------");
+		//Caso correcto
+		//cuenta=1 y el dni=12345678
+
+		Connection conn = null;
 		Statement st_check = null;
 		ResultSet rs = null;
-		
+
 		try {
 			altaAutorizado(1, 12345678);
-			
+
 			conn = p.getConnection();
-			st_check = conn.createStatement();			
+			st_check = conn.createStatement();
 			rs = st_check.executeQuery(
 					"SELECT * FROM cuentas_clientes " +
-					"WHERE idCli = 1 AND idCta = 1");
-			
-			if (rs.next()){
+							"WHERE idCli = 1 AND idCta = 1");
+
+			if (rs.next()) {
 				System.out.println("Caso todo correcto OK");
-			}
-			else{
+			} else {
 				System.out.println("Caso todo correcto  MAL");
 			}
-					
+
 		} catch (SQLException e) {
 			l.error(e.getMessage());
 		} finally {
-			
-			
+
+
 		}
 		System.out.println("--------------------------------");
-		//Pruebo que ese cliente ya exist�a con esa cuenta
+		//Pruebo que ese cliente ya exist?a con esa cuenta
 		//volviendo a insertar altaAutorizado(1, 12345678);
-		try{        	
-	    	altaAutorizado(1, 12345678);
-	    	System.out.println("Autorizaci�n repetida MAL");
-	     } catch (SQLException_PracticaJDBC2021 e) {
-			if (e.getErrorCode() == SQLException_PracticaJDBC2021.AUTORIZACION_REPETIDA)
-				System.out.println("Autorizaci�n repetida OK");			
+		try {
+			altaAutorizado(1, 12345678);
+			System.out.println("Autorizaci?n repetida MAL");
+		} catch (SQLException_banco e) {
+			if (e.getErrorCode() == SQLException_banco.AUTORIZACION_REPETIDA)
+				System.out.println("Autorizaci?n repetida OK");
 			else
-				System.out.println("Autorizaci�n repetida MAL");
+				System.out.println("Autorizaci?n repetida MAL");
 		}
-	    
+
 	}
 
-	public static void altaAutorizado( int cta, int dni) throws SQLException_PracticaJDBC2021{
-    	
-		
-		
-    	//A completar por el alumno
-    	
-		
-		
-    } 
+	public static void altaAutorizado(int cta, int dni) throws SQLException_banco {
+		// Inserta en CUENTAS_CLIENTES la asociación cta dni
+
+		PoolDeConexiones pool = PoolDeConexiones.getInstance();
+		Connection conn = null;
+
+		TableError errors = new OracleTableError();
+
+		PreparedStatement fetchCliente = null;
+		PreparedStatement insertaCliente = null;
+
+		ResultSet resCliente = null;
+		try {
+			// Obtén una conexión de la pool de conexiones.
+			conn = pool.getConnection();
+
+			fetchCliente = conn.prepareStatement("" +
+					" select IDCLIENTE" +
+					" from CLIENTES" +
+					" where DNI = ?");
+
+			insertaCliente = conn.prepareStatement("" +
+					" insert into CUENTAS_CLIENTES(IDCTA, IDCLI)" +
+					" values (?, ?)");
+
+			fetchCliente.setInt(1, dni);
+
+			resCliente = fetchCliente.executeQuery();
+
+			if (!resCliente.next()) {
+
+				throw new SQLException_banco(SQLException_banco.NO_EXISTE_CLIENTE);
+			} else {
+				insertaCliente.setInt(1, cta);
+				insertaCliente.setInt(2, resCliente.getInt("IDCLIENTE"));
+
+
+				try {
+					int rowCount = insertaCliente.executeUpdate();
+				} catch (SQLException e) {
+					/*
+					 * Capturo la Excepción en este bloque de código y no en el catch
+					 * exterior porque da una mayor flexibilidad en la aplicación en un futuro.
+					 *
+					 * Podría capturar un FK_VIOLATED en el catch exterior, pero si añado otra
+					 * sentencia que pueda lanzar el mismo error, el responder a ambos FK_VIOLATED
+					 * con "No existe ese nro de cuenta corriente" no es la mejor opción, ya que
+					 * puede estar relacionado con otra tabla totalmente distinta.
+					 * */
+
+					switch (errors.translate(e.getErrorCode())) {
+						case TableError.FK_VIOLATED:
+							throw new SQLException_banco(SQLException_banco.NO_EXISTE_CTA);
+						case TableError.UNQ_VIOLATED:
+							throw new SQLException_banco(SQLException_banco.AUTORIZACION_REPETIDA);
+						default:
+							throw e;
+					}
+				}
+			}
+
+			conn.commit();
+
+		} catch (SQLException_banco e) {
+			throw e;
+		} catch (SQLException e) {
+
+			if (conn != null) conn.rollback();
+			l.error(e.getMessage());
+			l.error(e.getLocalizedMessage());
+
+
+		} finally {
+			// Cierre de conexiones
+		}
+
+
+	}
+
 	public static boolean checkExceptionToCode(SQLException ex, int error) {
 		// return new
 		// OracleTableError().translate(e.getErrorCode())==TableError.FK_VIOLATED
 		return new OracleTableError().translate(ex.getErrorCode()) == error;
 	}
-    
-    static public void creaTablas() {
-		ExecuteScript.run(script_path+ "script_PracticaJDBC2021.sql");
+
+	static public void creaTablas() {
+		ExecuteScript.run(script_path + "script_PracticaJDBC2021.sql");
 	}
-        
+
 	static public void inicializaciones() {
 		try {
 			//Acuerdate de q la primera vez tienes que crear el .bindings con:
 			// PoolDeConexiones.reconfigurarPool();
-			
+
 			// Inicializacion de Pool
 			p = PoolDeConexiones.getInstance();
-			
+
 			// Inicializacion del Logger
-			l =	LoggerFactory.getLogger(PracticaJDBC2021.class);
+			l = LoggerFactory.getLogger(PracticaJDBC2021.class);
 
 		} catch (Exception e) {
 			System.err.println(e.getMessage());
---
+++ Entrega 03 - JDBC/PracticaJDBC2021/src/lsi/ubu/PracticaJDBC2021/SQLException_banco.java
@@ -0,0 +1,60 @@
+package lsi.ubu.PracticaJDBC2021;
+
+import java.sql.SQLException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SQLException_banco extends SQLException {
+
+
+	public static final int NO_EXISTE_CLIENTE = -1;
+	public static final int NO_EXISTE_CTA = -2;
+	public static final int AUTORIZACION_REPETIDA = -3;
+	private static final long serialVersionUID = 1L;
+	private static Logger l = null;
+	private final int codigo; // = -1;
+	private String mensaje;
+
+	public SQLException_banco(int code) {
+		codigo = code;
+
+
+		l = LoggerFactory.getLogger(SQLException_banco.class);
+		l.error(mensaje);
+
+		// Traza_de_pila
+		for (StackTraceElement ste : Thread.currentThread().getStackTrace()) {
+			l.info(ste.toString());
+		}
+	}
+
+	@Override
+	public String getMessage() { // Redefinicion del metodo de la clase
+		// Exception
+		return mensaje;
+	}
+
+	@Override
+	public int getErrorCode() { // Redefinicion del metodo de la clase
+		// SQLException
+		return codigo;
+	}
+
+	private void build_message() {
+		switch (this.codigo) {
+			case NO_EXISTE_CLIENTE:
+				mensaje = "No existe cliente con ese DNI";
+				break;
+			case NO_EXISTE_CTA:
+				mensaje = "No existe ese nro de cuenta corriente";
+				break;
+			case AUTORIZACION_REPETIDA:
+				mensaje = "Ese cliente ya había sido autorizado para esa cuenta";
+				break;
+			default:
+				mensaje = "Código de error desconocido";
+				break;
+		}
+	}
+}
